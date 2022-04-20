bded36bd51f11c4500c3f023baf8ee45633c694f
Tan Le <tanle2207@gmail.com>
2022-04-20 19:57:45 +0700

    Add Vue app to option page

---
+++ src/admin/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminApp'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/admin/admin-index.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import AdminApp from '@/admin/App.vue'
+
+const app = createApp(AdminApp)
+app.mount('#pry-admin')
